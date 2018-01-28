81a7f9c49b00548aef388de986bb60379d0a4a34
BearD01001 <dino@beard.ink>
2018-01-28 23:05:41 +0800

    Rename container component.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import container from './container.vue'
+import Container from './Container.vue'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#gravity-resume',
-  components: { container },
+  components: { Container },
   template: '<container/>',
 })
